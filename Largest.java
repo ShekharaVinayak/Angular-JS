d1ae53faf50bd4ee4a252052c34363b86b276801
ShekharaVinayak <shikarashekharavinayak@gmail.com>
2025-04-18 04:26:45 +0000

    Initial commit: Added Java files

---
+++ Largest.java
@@ -0,0 +1,8 @@
+public class Largest {
+    public static void main(String[] args) {
+        int a = 20, b = 30;
+        int max = (a > b) ? a : b;
+        System.out.println("Largest number is: " + max);
+    }
+}
+
