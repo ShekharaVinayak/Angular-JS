107e91b4f91541603ef1da3aace756df49696b47
ShekharaVinayak <shikarashekharavinayak@gmail.com>
2025-04-23 12:00:00 +0000

    Removed unwanted Java files

--- Largest.java
+++
@@ -1,8 +0,0 @@
-public class Largest {
-    public static void main(String[] args) {
-        int a = 20, b = 30;
-        int max = (a > b) ? a : b;
-        System.out.println("Largest number is: " + max);
-    }
-}
-
